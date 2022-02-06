b425871f6611149f1fe4ed2f4765e5d8092712db
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2022-02-06 15:45:01 +0700

    Added RadioInterface()

---
+++ src/radio_interface.py
@@ -0,0 +1,19 @@
+
+
+
+class RadioInterface(object):
+    def __init__(self):
+        pass
+        
+    def switch_on(self):
+        print("Playing radio")
+
+    def switch_off(self):
+        print("Turning off the radio")
+        
+    def next_channel(self):
+        print("Switching to next possible channel")
+    
+    def previous_channel(self):
+        print("Switching to previous possible channel")
+
