f42b5bde903ac5264f88400cb67314a5048e7121
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2022-02-13 10:39:37 +0700

    Test commit for moduleError fix

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from src.radio import *
 from src.connection import *
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
-from radio_interface import Radio
+from src.radio_interface
+
 
 class RadioApp(object):
 	def __init__(self):
-		pass
+		self.radio = src.radio_interface.Radio()
 		
 	def start(self):
 		pass
-		
-
+	
 
 def main():
 	radio_app = RadioApp()
 	radio_app.start()
 
 if __name__ == "__main__":
-
+	print("Starting program")
+	print(sys.path)
 	main()
