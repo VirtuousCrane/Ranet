deb33906a91622dead19fb453e1048d51fdb4407
Chatchai222 <65013350+Chatchai222@users.noreply.github.com>
2022-02-18 17:36:04 +0700

    Forgot main in prev commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from src.radio import Radio
 from src.gui.gui_design import MainGuiWindow, CSS
-from src.testcode import MyRadio
 
 class RadioApp(object):
 	def __init__(self):
@@ -14,13 +13,14 @@ class RadioApp(object):
 		self.gui = MainGuiWindow()
 
 		# Intiialize the radio
-		self.radio = MyRadio(self.gui)
+		self.radio = Radio(self.gui)
 		self.gui.set_radio_player(self.radio.get_radio_player())
 		
 		# Linking the radio model with the GUI callbacks
 		self.gui.set_play_button_callback(self.radio.toggle)
 		self.gui.set_previous_button_callback(self.radio.previous_channel)
 		self.gui.set_next_button_callback(self.radio.next_channel)
+		
 		
 	def start(self):
 		self.gui.show()
