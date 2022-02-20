fa83ff786cfedb645a0650d58a1c7338090ec0f6
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2022-02-20 21:29:24 +0700

    Volume Value, Channel List Gui (#15)
    * feat: control_channel_handler and hover effect
    
    * feat: call back function when button click && set default channel num and channel name
    
    * feat: callback function and toggle
    
    * feat: ControlChannelHandler callback
    
    * fix: farther modify
    
    * feat: add icon file
    
    * Revert "feat: add icon file"
    
    This reverts commit 06c431fb8153c768fe648e3ae7d5924f478c364a.
    
    * Revert "Revert "feat: add icon file""
    
    This reverts commit 14d8d2c97e99e36bb4c7af52c1c6628e634ec564.
    
    * Revert "Merge branch 'gui_design'"
    
    This reverts commit ebf414615ff296ad92e8008191399c604d2f210c, reversing
    changes made to 2876f8904cf60b26abd31e947dfa00fde0785686.
    
    * feat: impliment callback function and singleton
    
    * ControlClickedHandler
    change to singleton in order enable toggle
    * CreateMenuBar
    add trigger.connect function for Qaction is menu section
    
    add call back function for Qaction is menu file.
    
    * feat: volume slider value and callback
    
    * feat: channel list gui
    
    * feat: fix
    
    * feat: add and return channel function
    
    * feat: add image to button; icon toggle function
    
    * feat: relocate image file
    
    from src/gui/button_image -> asset folder
    
    Co-authored-by: Chayut Liewlom <ray.daroonpat.2020@gmail.com>

--- src/gui/gui_design.py
+++ src/gui/gui_design.py
@@ -3,6 +3,7 @@ from typing import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
+from radio_channel_list_gui import RadioChannelListGuiWindow
 
 class CreateMenuBar(QMainWindow):
 	def __init__(self):
@@ -23,13 +24,14 @@ class CreateMenuBar(QMainWindow):
 		self.next_action.triggered.connect(self.control_clicked_handler.next_button_click)
 		self.previous_action = QAction("&Previous", self)
 		self.previous_action.triggered.connect(self.control_clicked_handler.previous_button_click)
-		self.favorite_action = QAction("&Favorite", self)
+		self.channel_list_action = QAction("&Channel List", self)
+		self.channel_list_action.triggered.connect(self.open_channel_list_gui)
 
 		file_menu.addAction(self.play_action)
 		file_menu.addAction(self.pause_action)
 		file_menu.addAction(self.next_action)
 		file_menu.addAction(self.previous_action)
-		file_menu.addAction(self.favorite_action)
+		file_menu.addAction(self.channel_list_action)
 
 		self.menu_bar.addMenu(file_menu)
 
@@ -58,15 +60,19 @@ class CreateMenuBar(QMainWindow):
 	# file_menu callback Function
 	def set_play_action_callback(self, in_func):
 		self.play_action.clicked.connect(in_func)
-
+		
 	def set_pause_action_callback(self, in_func):
 		self.pause_action.clicked.connect(in_func)
-
+		
 	def set_previous_action_callback(self, in_func):
 		self.previous_action.clicked.connect(in_func)
-
+		
 	def set_next_action_callback(self, in_func):
 		self.next_action.clicked.connect(in_func)
+
+	def open_channel_list_gui(self):
+		self.channel_list_gui = RadioChannelListGuiWindow()
+		self.channel_list_gui.show()
 
 class CreateChannelWave(QWidget):
 	def __init__(self):
@@ -118,98 +124,83 @@ class CreateControlBar(QWidget):
 		self.control_button_layout.setAlignment(Qt.AlignCenter)
 		self.control_button_layout.setContentsMargins(0,8,0,0)
 		self.control_button_layout.setSpacing(8)
-
+		
 		self.control_clicked_handler = ControlClickHandler()
 
-		# Buttons Play, Next, Previous
-		## Previous Button
-		self.previous_button = QPushButton("Previous", self)
+	  # Buttons Play, Next, Previous
+		# Previous Button
+		self.previous_button = QPushButton(self)
+		self.previous_button.setIcon(QIcon(QPixmap("assets/previous_icon.png")))
+		self.previous_button.setIconSize(QSize(24,24))
 		self.previous_button.setFixedSize(44,44)
 		self.previous_button.setStyleSheet("border-radius: 22; border: 2px solid black")
-		self.previous_button.clicked.connect(self.previous_button_callback)
-
+		self.previous_button.clicked.connect(self.control_clicked_handler.previous_button_click)
+		
 		self.control_button_layout.addWidget(self.previous_button)
 
-		## Play Button
-		self.play_button = QPushButton("Play", self)
+		# Play Button
+		self.play_button = QPushButton(self)
+		self.play_button.setIcon(QIcon(QPixmap("assets/play_icon.png")))
+		self.play_button.setIconSize(QSize(28,28))
 		self.play_button.setFixedSize(50,50)
 		self.play_button.setStyleSheet("border-radius: 25; border: 2px solid black;")
-		self.play_button.clicked.connect(self.play_button_callback)
-
+		self.play_button.clicked.connect(self.control_clicked_handler.play_pause_button_click)
+		self.play_button.clicked.connect(self.toggle_play_pause_icon)
+		
 		self.control_button_layout.addWidget(self.play_button)
-
-		## Next Button
-		self.next_button = QPushButton("Next", self)
+    
+		# Next Button
+		# self.next_button = QPushButton("Next", self)
+		self.next_button = QPushButton(self)
+		self.next_button.setIcon(QIcon(QPixmap("assets/next_icon.png")))
+		self.next_button.setIconSize(QSize(24,24))
 		self.next_button.setFixedSize(44,44)
 		self.next_button.setStyleSheet("border-radius: 22; border: 2px solid black;")
-		self.next_button.clicked.connect(self.next_button_callback)
-
+		self.next_button.clicked.connect(self.control_clicked_handler.next_button_click)
+		
 		self.control_button_layout.addWidget(self.next_button)
 
 		self.control_layout.addLayout(self.control_button_layout)
-
-		# Sound Volume Control
+		
+	  # Sound Volume Control
 		self.volume_slider = QSlider(Qt.Horizontal)
 		self.volume_slider.setFixedSize(154, 20)
+		self.volume_slider.setMinimum(0)
+		self.volume_slider.setMaximum(100)
 
+		# Test Slider Volume Change
+		self.volume_slider.valueChanged.connect(self.volume_test_run)
+		
 		self.control_layout.addWidget(self.volume_slider)
 
-	def play_button_callback(self):
-		"""The callback of the play button"""
-		print("Play button Click")
-
-	def previous_button_callback(self):
-		"""The callback of the previous button"""
-		print("Previous button Click")
-
-	def next_button_callback(self):
-		"""The callback of the next button"""
-		print("Next button Click")
-
-	def set_play_button_callback(self, in_func: Callable):
-		"""
-		Sets the callback of the play button
-
-		Parameters
-		----------
-		in_func : Callable
-			The new play button callback
-		"""
+		# Toggle Play Pause Icon
+	def toggle_play_pause_icon(self):
+		if (self.control_clicked_handler.currently_playing == True):
+			self.play_button.setIcon(QIcon(QPixmap("assets/pause_icon.png")))
+			self.play_button.setIconSize(QSize(24,24))
+		else: 
+			self.play_button.setIcon(QIcon(QPixmap("assets/play_icon.png")))
+			self.play_button.setIconSize(QSize(28,28))
+		
+	def set_play_button_callback(self, in_func):
 		self.play_button.clicked.connect(in_func)
-
-	def set_previous_button_callback(self, in_func: Callable):
-		"""
-		Sets the callback of the previous button
-
-		Parameters
-		----------
-		in_func : Callable
-			The new previous button callback
-		"""
+		
+	def set_previous_button_callback(self, in_func):
 		self.previous_button.clicked.connect(in_func)
-
-	def set_next_button_callback(self, in_func: Callable):
-		"""
-		Sets the callback of the next button
-
-		Parameters
-		----------
-		in_func : Callable
-			The new next button callback
-		"""
+		
+	def set_next_button_callback(self, in_func):
 		self.next_button.clicked.connect(in_func)
-
-	def set_volume_slider_callback(self, in_func: Callable):
-		"""
-		Sets the callback of the volume slider
-
-		Parameters
-		----------
-		in_func : Callable
-			The new volume slider callback
-		"""
+		
+	def set_volume_slider_callback(self, in_func):
 		self.volume_slider.valueChanged.connect(in_func)
 
+	def set_volume_slider_value(self, in_func):
+		self.volume_slider.setValue(in_func)
+
+	# Volume Value Test
+	def volume_test_run(self):
+		print(self.volume_slider.value())
+		
 class MainGuiWindow(QMainWindow):
 	def __init__(self, radio_player=None):
 		QMainWindow.__init__(self, None)
@@ -218,6 +209,7 @@ class MainGuiWindow(QMainWindow):
 
 		self.create_menu_bar = CreateMenuBar()
 		self.setMenuBar(self.create_menu_bar.menu_bar)
+
 
 		self.radio_player = radio_player
 
@@ -271,8 +263,6 @@ class ControlClickHandler(object):
 	def __new__(self):
 		if not self._instance:
 			self._instance = super(ControlClickHandler, self).__new__(self)
-			# QWidget.__init__(self, None)
-			# Set Default Status as True (Currently Playing)
 			self.currently_playing = False
 		return self._instance
 
